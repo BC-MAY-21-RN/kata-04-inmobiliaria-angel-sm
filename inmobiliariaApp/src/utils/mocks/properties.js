93d0a944d757ce22ac80dd1f40140d3dc74a44eb
angel-sm <josesanchez@solemti.mx>
2021-06-21 00:42:48 -0500

    Update: Mocks
    Change mocks info for get better coverage in codeclimate

--- inmobiliariaApp/src/utils/mocks/properties.js
+++ inmobiliariaApp/src/utils/mocks/properties.js
@@ -1,127 +1,68 @@
+const ADDRESS = '3517 W. Gray St. Utica'
+const PRICE = 440000
+const CHARACTERISTICS =  [
+  {
+    name: "beds",
+    total: 3,
+    icon: 'https://i.imgur.com/6WfprKV.png'
+  },
+  {
+    name: "bathrooms",
+    total: 2,
+    icon: 'https://i.imgur.com/wCNO6xX.png'
+  },
+  {
+    name: "size",
+    total: 230,
+    icon: 'https://i.imgur.com/TRT4kVh.png'
+  },
+]
+const IMAGE = 'https://i.imgur.com/zbAmEfA.png'
+
 export const properties = [
   {
     id: 1,
-    name: 'The Willows 1',
-    address: '3517 W. Gray St. Utica',
-    characteristics: [
-      {
-        name: "beds",
-        total: 3,
-        icon: 'https://i.imgur.com/6WfprKV.png'
-      },
-      {
-        name: "bathrooms",
-        total: 2,
-        icon: 'https://i.imgur.com/wCNO6xX.png'
-      },
-      {
-        name: "size",
-        total: 230,
-        icon: 'https://i.imgur.com/TRT4kVh.png'
-      },
-    ],
-    price: 440000,
-    image: 'https://i.imgur.com/zbAmEfA.png', 
+    name: 'The Willows',
+    address: ADDRESS,
+    characteristics: CHARACTERISTICS,
+    price: PRICE,
+    image: IMAGE, 
     ranking: 4.5
   },
   {
     id: 2,
-    name: 'The Willows 2',
-    address: '3517 W. Gray St. Uticat',
-    characteristics: [
-      {
-        name: "beds",
-        total: 3,
-        icon: 'https://i.imgur.com/6WfprKV.png'
-      },
-      {
-        name: "bathrooms",
-        total: 2,
-        icon: 'https://i.imgur.com/wCNO6xX.png'
-      },
-      {
-        name: "size",
-        total: 230,
-        icon: 'https://i.imgur.com/TRT4kVh.png'
-      },
-    ],
-    price: 440000,
-    image: 'https://i.imgur.com/zbAmEfA.png', 
+    name: 'The Willows',
+    address: ADDRESS,
+    characteristics: CHARACTERISTICS,
+    price: PRICE,
+    image: IMAGE, 
     ranking: 4.1
   },
   {
     id: 3,
     name: 'The Siattle',
-    address: '3517 W. Gray St. Utican',
-    characteristics: [
-      {
-        name: "beds",
-        total: 3,
-        icon: 'https://i.imgur.com/6WfprKV.png'
-      },
-      {
-        name: "bathrooms",
-        total: 2,
-        icon: 'https://i.imgur.com/wCNO6xX.png'
-      },
-      {
-        name: "size",
-        total: 230,
-        icon: 'https://i.imgur.com/TRT4kVh.png'
-      },
-    ],
-    price: 440000,
-    image: 'https://i.imgur.com/zbAmEfA.png', 
+    address: ADDRESS,
+    characteristics: CHARACTERISTICS,
+    price: PRICE,
+    image: IMAGE, 
     ranking: 4.2
   },
   {
     id: 4,
     name: 'Corner House',
-    address: '3517 W. Gray St. Uticas',
-    characteristics: [
-      {
-        name: "beds",
-        total: 3,
-        icon: 'https://i.imgur.com/6WfprKV.png'
-      },
-      {
-        name: "bathrooms",
-        total: 2,
-        icon: 'https://i.imgur.com/wCNO6xX.png'
-      },
-      {
-        name: "size",
-        total: 230,
-        icon: 'https://i.imgur.com/TRT4kVh.png'
-      },
-    ],
-    price: 440000,
-    image: 'https://i.imgur.com/zbAmEfA.png', 
+    address: ADDRESS,
+    characteristics: CHARACTERISTICS,
+    price: PRICE,
+    image: IMAGE, 
     ranking: 4.9
   },
   {
     id: 5,
     name: 'Orchard House',
-    address: '3517 W. Gray St. Uticad',
-    characteristics: [
-      {
-        name: "beds",
-        total: 3,
-        icon: 'https://i.imgur.com/6WfprKV.png'
-      },
-      {
-        name: "bathrooms",
-        total: 2,
-        icon: 'https://i.imgur.com/wCNO6xX.png'
-      },
-      {
-        name: "size",
-        total: 230,
-        icon: 'https://i.imgur.com/TRT4kVh.png'
-      },
-    ],
-    price: 440000,
-    image: 'https://i.imgur.com/zbAmEfA.png', 
+    address: ADDRESS,
+    characteristics: CHARACTERISTICS,
+    price: PRICE,
+    image: IMAGE, 
     ranking: 4.8
   },
 ];
